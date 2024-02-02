87e6dfc9d0c5de6d40b0a3027e6e71cd3be0e7e1
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-02-01 21:29:03 -0300

    Remove unnecessary code from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { Container, Main } from '@/components/sharedstyles'
 import { Navbar } from '@/components/navbar'
 import { NavBarLinks } from '@/constants/menu'
@@ -7,11 +6,6 @@ import { Banner } from '@/components/banner'
 export default function Home() {
   return (
     <Container>
-      <Head>
-        <title>Junior Schmidt</title>
-        <meta name="description" content="Explorando novas tecnologias e desenvolvendo perspicácia em programação. Conheça o portfólio de Junior Schmidt, repleto de projetos inovadores que refletem sua paixão pela programação e suas habilidades em constante evolução." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Main>
         <Navbar
           NavBarLinks={NavBarLinks}
