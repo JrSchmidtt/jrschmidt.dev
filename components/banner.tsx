0e7fd24a1e2c3f3c22903c105b561543480fdce8
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-05-10 17:17:07 -0300

    chore: Refactor banner component, add button, and update styles

--- components/banner.tsx
+++ components/banner.tsx
@@ -2,38 +2,56 @@ import styled from 'styled-components'
 
 const Banner = () => {
     return (
-        <>
-            <Container>
-                <CallToAction>
-                    <h3>HELLO</h3>
-                    <h1>I AM JUNIOR SCHMIDT</h1>
-                    <p>Explorando novas tecnologias e desenvolvendo perspicácia em programação.</p>
-                </CallToAction>
+        <Container>
+            <CallToAction>
+                <h3>HELLO</h3>
+                <h1>I AM JUNIOR SCHMIDT</h1>
+                <p>Explorando novas tecnologias e desenvolvendo perspicácia em programação.</p>
                 <Button>VER PROJETOS</Button>
-            </Container>
-        </>
+            </CallToAction>
+            <Profile>
+                <img src='/images/junior-schmidt.png' alt="Junior Schmidt Profile" />
+                <div>
+                    <p>Desenvolvedor Full-Stack</p>
+                </div>
+            </Profile>
+        </Container>
     )
 }
 
+const Profile = styled.div`
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    img {
+        width: 400px;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+`;
+
 const Button = styled.button`
-    background-color: #1f2447;
-    border: 1px solid #000;
+    background-color: ${({ theme }) => theme.colors.primary};
     padding: 10px 20px;
     border-radius: 5px;
-    font-size: 1.2rem;
-    font-weight: 700;
-    color: #000;
     cursor: pointer;
-    transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease;
     &:hover {
-        background-color: #000;
+        background-color: ${({ theme }) => theme.colors.secondary};
         color: #fff;
+        transform: scale(1.1);
     }
 `;
 
 const Container = styled.div`
  width: 100%;
- height: 100vh;
+height: 100vh;
+ display: flex;
+ flex-direction: row;
+ align-items: center;
+ justify-content: center;
 `
 
 const CallToAction = styled.div`
@@ -42,6 +60,7 @@ const CallToAction = styled.div`
     justify-content: center;
     align-items: center;
     height: 100%;
+    width: 70%;
     h3 {
         font-size: 1.5rem;
         color: #fff;
