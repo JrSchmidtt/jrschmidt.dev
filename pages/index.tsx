f4001d061e57a76334a3feed3047309e716a281b
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-08-09 18:36:54 -0300

    refactor: Update landing page with dynamic data from GitHub API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,49 @@
-import { Container, Main } from '@/components/sharedstyles'
-import { Navbar } from '@/components/navbar'
-import { NavBarLinks } from '@/constants/menu'
-import { Banner } from '@/components/banner'
+import { Container, Section } from "@/components/sharedstyles";
+import { Navbar } from "@/components/navbar";
+import { NavBarLinks } from "@/constants/menu";
+import { Banner } from "@/components/banner";
+import { getFromGithub } from "@/lib/Api";
+import useSWR from "swr";
+import Card from '@/components/card';
+import Grid from "@/components/grid";
 
 export default function Home() {
+  const { data, error } = useSWR("/users/JrSchmidtt/repos", (url: string) =>
+    getFromGithub(url, 0, 22)
+  );
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <Container>
-      <Main>
-        <Navbar
-          NavBarLinks={NavBarLinks}
-        />
-        <Banner />
-      </Main>
-    </Container>
-  )
+    <>
+      <Container backgroundType="image" backgroundColour="primary">
+        <Section>
+          <Navbar NavBarLinks={NavBarLinks} />
+          <Banner />
+        </Section>
+      </Container>
+      <Container>
+        <Section>
+          <h3>Open Source Projects</h3>
+          <Grid>
+          {data.map((repo: any) => (
+            <Card key={repo.id}>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                <h4>{repo.name}</h4>
+              </a>
+              <p>{repo.description}</p>
+              <ul>
+                <li>Lang: {repo.language ?? "None"}</li>
+                <li>Stars: {repo.stargazers_count}</li>
+                <li>License: {repo.license?.name ?? "None"}</li>
+                <li>Tags: {repo.topics.join(", ")}</li>
+                <li>Homepage: {repo.homepage ?? "None"}</li>
+              </ul>
+            </Card>
+          ))}
+          </Grid>
+        </Section>
+      </Container>
+    </>
+  );
 }
