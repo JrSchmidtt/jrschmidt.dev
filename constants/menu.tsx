56b465f6b4a252572da0530b609556193234dcbd
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-08-09 14:09:44 -0300

    feat: Atualizar links do menu e adicionar link para o WhatsApp

--- constants/menu.tsx
+++ constants/menu.tsx
@@ -1,9 +1,8 @@
 import { INavBarLinks } from '../components/navbar'
 
 export const NavBarLinks = [
-    { Name: 'Sobre', url: '/about' },
-    { Name: 'Blog', url: '/blog' },
-    { Name: 'Youtube', url: 'https://www.youtube.com/channel/UC9yh45t4tnB57gE-FXu5tOA', target: '_blank'},
+    { Name: 'Blog', url: '#' },
     { Name: 'Github', url: 'https://github.com/JrSchmidtt', target: '_blank' },
-    { Name: 'Linkedin', url: 'https://www.linkedin.com/in/jrschmidtt/', target: '_blank' }
+    { Name: 'Linkedin', url: 'https://www.linkedin.com/in/jrschmidtt/', target: '_blank' },
+    { Name: 'WhatsApp', url: 'https://web.whatsapp.com/send/?phone=5541995880804&text=Ol%C3%A1%2C+Junior+Schmidt+estava+navegando+em+seu+portif%C3%B3lio%2C+gostaria+de+entrar+em+contato.', target: '_blank' },
 ] as INavBarLinks[]
