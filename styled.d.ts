5994a0943ae22f4013b19eb875364d7fab79b452
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-08-09 18:36:18 -0300

    style: Update styled theme with new colors

--- styled.d.ts
+++ styled.d.ts
@@ -3,10 +3,15 @@ import 'styled-components'
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
+      textSecondary: string
+      link: string
+      highlightBackground: string
+      highlight: string
       text: string
       background: string
       primary: string
       secondary: string
+      cardBackground: string
     }
   }
 }
