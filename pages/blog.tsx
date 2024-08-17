7322662b0f32d00c28fc1b86ed1543ea04e1af56
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-08-17 11:50:47 -0300

    refactor: Update blog page to display articles with improved styling and dynamic data

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,39 +1,44 @@
 import { Container, Section } from "@/components/sharedstyles";
-import { Navbar } from "@/components/navbar";
-import { NavBarLinks } from "@/constants/menu";
-import { Banner } from "@/components/banner";
 import { getArticles } from "@/lib/Api";
 import useSWR from "swr";
-import Card from '@/components/card';
+import Card from "@/components/card";
 import Grid from "@/components/grid";
+import { FormatDate } from "@/utils/data";
 
 export default function Home() {
   const { data, error } = useSWR("jrschmidtt", (username: string) =>
     getArticles(username)
   );
   if (error) return <div>Failed to load</div>;
-  console.log(data)
 
   return (
-      <Container>
-        <Section>
-          <h3>My Public Notes</h3>
-          <Grid>
-          {data && data.map((article: any) => (
-            <Card key={article.id}>
-              <img src={article.cover_image} alt={article.title} />
-              <a href={article.html_url} target="_blank" rel="noopener noreferrer">
-                <h4>{article.title}</h4>
-              </a>
-              <p>{article.description}</p>
-              <ul>
-                <li>Published: {article.published_at}</li>
-                <li>Reading Time: {article.reading_time_minutes} minutes</li>
-              </ul>
-            </Card>
-          ))}
-          </Grid>
-        </Section>
-      </Container>
+    <Container>
+      <Section>
+        <h3>My Public Notes</h3>
+        <Grid>
+          {data &&
+            data.map((article: any) => (
+              <Card key={article.id}>
+                <a href={article.url} target="_blank" rel="noopener noreferrer">
+                  <img src={article.cover_image} alt={article.title} />
+                  <h4>{article.title}</h4>
+                </a>
+                <p>{article.description}</p>
+                <ul>
+                  <li>
+                    Published: {FormatDate(article.published_at, "pt-br")}
+                  </li>
+                  <li>Reading Time: {article.reading_time_minutes} minutes</li>
+                </ul>
+                <ul>
+                  {article.tag_list.map((tag: string) => (
+                    <li key={tag}>#{tag}</li>
+                  ))}
+                </ul>
+              </Card>
+            ))}
+        </Grid>
+      </Section>
+    </Container>
   );
 }
